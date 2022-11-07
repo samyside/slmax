9a440b928a04e28a64d6a72b9b0a49786a672b6c
samyside <samyside@yandex.ru>
2022-11-07 22:43:04 +0300

    formatted code for testing

--- index.php
+++ index.php
@@ -12,17 +12,42 @@
 
 include 'UserArray.php';
 
-// Создание пользователя только по полям
-$userById = new User('Steve', 'Howking', '2000-01-02', 1, 'Brest');
-$userById->remove(3);
-echo $userById->show();
+try {
+	echo "\n---> Создание пользователя только по полям\n";
+	$userByFields = new User('Steve', 'Howking', '2000-01-02', 1, 'Brest');
+	var_dump($userByFields);
 
-// Получение пользователя по id
-// $userByFields = new User(2);
+	echo "\n---> Получение пользователя из базы данных по id\n";
+	$userById = new User(3);
+	var_dump($userById);
 
-// Создание списка людей по массиву id
-// $arrayUsers = new UserArray(array(1, 2, 3, 4, 7));
+	echo "\n---> Попытка получить несуществующего пользователя в базе данных\n";
+	$userById = new User(999);
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
 
-// Удаление пользователей по в соответствии с переданным массивом id
-// $arrayUsers->removeUsers();
+echo "\n---> Удаление пользователя по id\n";
+User::remove(4);
 
+echo "\n---> Преобразование даты рождения в возраст\n";
+echo User::convertDateToAge('2000-01-02');
+
+echo "\n---> Преобразование значение поля 'пол' из двоичной системы в строковое 'муж' или 'жен'\n";
+echo "--> " . User::convertSex(0) . "\n";
+echo "--> " . User::convertSex(1) . "\n";
+
+echo "\n---> Форматирование пользователя с преобразованием возраста и (или) пола\n";
+$objectStd = $userByFields->format(0);
+var_dump($objectStd);
+$objectStd = $userByFields->format('2000-02-01');
+var_dump($objectStd);
+$objectStd = $userByFields->format(1, '1998-04-10');
+var_dump($objectStd);
+
+$userArray = new UserArray(array(1, 3, 5, 6));
+$listUsers = $userArray->getUsers();
+var_dump($listUsers);
+
+echo "\n---> Удаление пользователей в соответствии с переданным массивом\n";
+$userArray->removeUsers();
