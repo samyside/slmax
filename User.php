460eb4c4490b79b7dba5f507476ff2366cf2b40e
samyside <samyside@yandex.ru>
2022-11-07 13:45:02 +0300

    add validation in User::__constructor() by id

--- User.php
+++ User.php
@@ -1,6 +1,29 @@
 <?php
+/**
+ * TODO:
+ * [+] Проверять в конструкторе, доступен ли указанный id
+ * 
+ * Автор: Влад Бабенко
+ * 
+ * Дата реализации: 05.11.2022 11:00
+ * 
+ * Дата изменения: 07.11.2022 10:30
+ * 
+ * Описание сущности User
+ */
+
 include_once 'Database.php';
 
+/*
+Класс User создает сущность с полями: id, имя, фамилия, дата рождения, пол, город рождения.
+Принимает в конструкторе либо одно поле с id, либо с полями: имя, фамилия, дата рождения, пол, город рождения. В случае передачи id, читает запись из БД с указанным id. В случае передачи полей, создает новую запись в БД с указанными полями.
+Методы класса:
+- remove(int). Удаляет запись из БД по id
+- convertDateToAge(string). Получает возраст исходя из даты рождения
+- getSex(bool). Получает строковое значение пола исходя из булевского значения
+- show(). Получает строковое значение всех полей
+- format(). Возвращает новый экземпляр stdClass с преобразованными полями возраста и пола.
+*/
 class User {
 	private $id;
 	private $name;
@@ -15,8 +38,11 @@ class User {
 
 		if ($args_count === 1) {
 			$this->id = $args[0];
-			$sql = "SELECT name, surname, birthdate, sex, native_city FROM users WHERE id={$this->id};";
-			// TODO Проверять, доступен ли указанный id
+			if (!$this->isExists($this->id)) {
+				echo 'HERE IS Exception' . PHP_EOL;
+				throw new Exception("Error! User is undefined by id($this->id)");
+			}
+			$sql = "SELECT name, surname, birthdate, sex, native_city FROM users WHERE id={$this->id}";
 			$set = Database::query($sql);
 			$row = $set->fetch_assoc();
 			$this->name       = $row['name'];
@@ -30,22 +56,29 @@ class User {
 			$this->birthdate  = $args[2];
 			$this->sex        = $args[3];
 			$this->nativeCity = $args[4];
+			$this->save();
 		} else {
 			throw new Exception("Error! Wrong amount of parameters in __construct()\n", 1);
 		}
-
-		$this->save();
 	}
 
 	private function save()
 	{
-		if (isset($this->id)) {
-			$this->edit($this->id);
-		} else {
-			$this->create($this);
-		}
+		$sql = "INSERT INTO users "
+			. "(name, surname, birthdate, sex, native_city) "
+			. "VALUES ('{$this->name}', '{$this->surname}', "
+			. "'{$this->birthdate}', {$this->sex}, '{$this->nativeCity}')";
+		Database::query($sql);
 	}
 
+	public function isExists(int $id)
+	{
+		$sql = "SELECT id FROM users WHERE id=$id";
+		echo 'NUM_ROWS: ' . Database::query($sql)->num_rows . PHP_EOL;
+		return Database::query($sql)->num_rows > 0;
+	}
+
+	// Deprecated
 	private function edit()
 	{
 		$sql = "UPDATE users SET "
@@ -54,17 +87,18 @@ class User {
 			. "birthdate='{$this->birthdate}', "
 			. "sex='{$this->sex}', "
 			. "native_city='{$this->nativeCity}' "
-			. "WHERE id={$this->id};";
+			. "WHERE id={$this->id}";
 
 		Database::query($sql);
 	}
 
+	// Deprecated
 	private function create()
 	{
 		$sql = "INSERT INTO users "
 			. "(name, surname, birthdate, sex, native_city) "
 			. "VALUES ('{$this->name}', '{$this->surname}', "
-			. "'{$this->birthdate}', {$this->sex}, '{$this->nativeCity}');";
+			. "'{$this->birthdate}', {$this->sex}, '{$this->nativeCity}')";
 		Database::query($sql);
 	}
 
@@ -74,12 +108,23 @@ class User {
 		Database::query($sql);
 	}
 
-	public static function getAge() : int
+	public function getBirthdate()
 	{
-		return date_diff(date_create($this->birthdate, date_create('now')))->y;
+		return $this->birthdate;
 	}
 
-	public static function getSex($sex) : string
+	public function getSex()
+	{
+		return $this->sex;
+	}
+
+	public static function convertDateToAge($birthdate) : int
+	{
+		echo 'birthdate=' . $birthdate . PHP_EOL;
+		return date_diff(date_create($birthdate), date_create('now'))->y;
+	}
+
+	public static function convertSex($sex) : string
 	{
 		return $sex ? 'муж' : 'жен';
 	}
@@ -88,15 +133,22 @@ class User {
 	{
 		return "{id:$this->id; "
 			. "name:$this->name; "
-			. "nativeCity:$this->nativeCity"
+			. "nativeCity:$this->nativeCity; "
+			. "birthdate:$this->birthdate"
 			. "}\n";
 	}
 
 	public function format() : stdClass
 	{
-		$obj = (object) get_object_vars($this);
-		$obj->age = self::getAge();
-		$obj->sex = self::getSex();
+		$obj = new stdClass;
+		$obj->id = $this->id;
+		$obj->name = $this->name;
+		$obj->surname = $this->surname;
+		$obj->birthdate = $this->birthdate;
+		$obj->sex = $this->sex;
+		$obj->nativeCity = $this->nativeCity;
+		$obj->age = self::convertDateToAge($this->getBirthdate());
+		$obj->sex = self::convertSex($this->getSex());
 		return $obj;
 	}
 }
--- index.php
+++ index.php
@@ -1,24 +1,43 @@
 <?php
 
-/*
-Автор:
+/**
+Автор: Влад Бабенко
 
-Дата реализации: 05.11.2022
+Дата реализации: 05.11.2022 11:00
 
-Дата изменения: 05.11.2022
+Дата изменения: 07.11.2022 10:30
 
-Утилита для работы с базой данных "mysql" (консольная утилита GNU/Linux)
+Точка входа программы
 */
 
 include 'UserArray.php';
 
-$user1 = new User('Steve', 'Howking', 2000, 1, 'Brest');
+$user1 = new User('Steve', 'Howking', '2000-01-02', 1, 'Brest');
 echo $user1->show();
-$user2 = new User(1);
+$user2 = new User(2);
 
-$array_users = new UserArray([1,2,3,4, 10]);
+$array_users = new UserArray(array(1, 2, 3, 4, 7));
 
 foreach ($array_users->getUsers() as $user) {
 	echo $user->show();
 }
-$array_users->removeUsers();
+// $array_users->removeUsers();
+
+$user3 = new User(3);
+var_dump($user3);
+$user3 = $user3->format();
+echo User::convertDateToAge($user2->getBirthdate());
+var_dump($user3);
+// var_dump($user1, $user3);
+
+try {
+	$user100 = new User(100);
+	echo 'isExists() : ' . $user100->isExists($user100->id) . PHP_EOL;
+	var_dump($user100);
+} catch (Exception $e) {
+	// echo "Undefined user by id($user100->id)" . PHP_EOL;
+	var_dump($e);
+	echo $e->getMessage() . PHP_EOL;
+}
+
+echo 'FINISH.' . PHP_EOL;
